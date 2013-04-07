eeb665d876b10795b440ba18c406fc7218f1e1fc
bryceAebi <bryce_aebi@brown.edu>
2013-04-07 18:19:32 -0400

    some updates to blocks

--- src/backend/blocks/Bracket.java
+++ src/backend/blocks/Bracket.java
@@ -1,11 +1,27 @@
 package backend.blocks;
 
-class Bracket implements Numerical
-{
-	private boolean open;
 
-	public boolean isOpen()
-	{
-		return open;
+/** A Bracket numerical
+ * 
+ * @author baebi
+ */
+public class Bracket implements Numerical {
+	private boolean _isOpen;
+	
+	/** Constructor for bracket
+	 * 
+	 * @param isOpen true if this is an open bracket, else false
+	 */
+	public Bracket(boolean isOpen){
+		_isOpen = isOpen;
+	}
+	
+	
+	/** Returns the open/close status of this Bracket
+	 * 
+	 * @return true if it is an open bracket, else false
+	 */
+	public boolean isOpen(){
+		return _isOpen;
 	}
 }
--- src/backend/blocks/Countable.java
+++ src/backend/blocks/Countable.java
@@ -1,6 +1,27 @@
 package backend.blocks;
 
-abstract class Countable implements Numerical
-{
+/** A number value object. This includes scalar values
+ *  and matrices
+ */
+abstract class Countable implements Numerical{
+	private boolean _isDouble, _isFraction;
+	
+	/** Retrieves fraction status of this countable
+	 * 
+	 * @return true if the value(s) of this object should be represented
+	 * as fraction(s)
+	 */
+	public boolean isFraction(){
+		return _isFraction;
+	}
+	
+	/** Retrieves double status of this countable
+	 * 
+	 * @return true if the value(s) of this object should be represented
+	 * as decimal(s)
+	 */
+	public boolean isDouble(){
+		return _isDouble;
+	}
 
 }
