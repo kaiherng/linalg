279caa37a559538f78d06bbe054cba5dcbc866e8
jonathan <jonathanysp@gmail.com>
2013-04-13 15:29:16 -0400

    Added matrixShape class to draw matrices

--- src/frontend/containers/Solution.java
+++ src/frontend/containers/Solution.java
@@ -3,14 +3,27 @@ package frontend.containers;
 import frontend.general.Constants;
 import frontend.graphicsengine.Container;
 import frontend.shapes.Coord;
+import frontend.shapes.MatrixShape;
 import frontend.shapes.Text;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
+import backend.blocks.Countable;
+import backend.blocks.Matrix;
+
 public class Solution extends Container {
+	
+	MatrixShape _ms;
 
 	public Solution() {
 		super(false, 1);
+		Matrix m = new Matrix(Countable.DisplayType.WHOLENUMBER, 2, 2);
+		m.setValueIndex(1, 0, 0);
+		m.setValueIndex(2, 0, 1);
+		m.setValueIndex(3, 1, 0);
+		m.setValueIndex(4, 1, 1);
+		_ms = new MatrixShape(m, 30, new Coord(0,0), this);
 	}
 	
 	@Override
@@ -26,6 +39,7 @@ public class Solution extends Container {
 	@Override
 	public void onDraw(Graphics2D g) {
 		super.onDraw(g);
+		_ms.onDraw(g);
 	}
 
 }
---
+++ src/matrixDraw/MatrixDraw.java
@@ -0,0 +1,73 @@
+package matrixDraw;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
+
+import backend.blocks.Matrix;
+
+public class MatrixDraw extends JPanel{
+	
+	Double[][] _values;
+	TeXIcon ti_;
+
+	public MatrixDraw(Matrix m) {
+		_values = m.getValues();
+		TeXFormula f = new TeXFormula(getLatexWhole());
+		ti_ = f.createTeXIcon(TeXConstants.STYLE_DISPLAY, 30);
+		this.setPreferredSize(new Dimension(ti_.getIconWidth(), ti_.getIconHeight()));
+	}
+	
+	public void paint(Graphics g){
+		ti_.paintIcon(new JLabel(), g, 0, 0);
+	}
+	
+	public String getLatex(){
+		StringBuilder b = new StringBuilder();
+		b.append("\\begin{pmatrix} ");
+		for(Double[] i : _values){
+			for(Double j : i){
+				b.append(j.toString());
+				if(!j.equals(i[i.length-1])){
+					b.append(" & ");
+				}
+			}
+			if(!i.equals(_values[_values.length - 1])){
+				b.append("\\\\");
+			}
+		}
+		b.append("\\end{pmatrix}");
+		System.out.println(b.toString());
+		return b.toString();
+	}
+	
+	public String getLatexWhole(){
+		StringBuilder b = new StringBuilder();
+		b.append("\\begin{pmatrix} ");
+		for(Double[] i : _values){
+			for(Double j : i){
+				b.append(j.intValue());
+				if(!j.equals(i[i.length-1])){
+					b.append(" & ");
+				}
+			}
+			if(!i.equals(_values[_values.length - 1])){
+				b.append("\\\\");
+			}
+		}
+		b.append("\\end{pmatrix}");
+		System.out.println(b.toString());
+		return b.toString();
+	}
+
+}
