ab2f5e0ffb4f3138c5996cf50ebc1acfe20944ef
Kai Loh <kai_herng_loh@brown.edu>
2013-04-29 17:46:10 -0400

    Implement resize for southwest and southeast

--- src/frontend/swing/ResizeAdapter.java
+++ src/frontend/swing/ResizeAdapter.java
@@ -1,7 +1,6 @@
 package frontend.swing;
 
 import java.awt.Component;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Insets;
@@ -17,8 +16,8 @@ import javax.swing.SwingUtilities;
 public class ResizeAdapter extends MouseAdapter{
 
 	private int _resizing = 0;
-    private int prevX = -1;
-    private int prevY = -1;
+    private int _prevX = -1;
+    private int _prevY = -1;
     private JFrame _frame;
     private int _threshold;
     private Rectangle _prevBounds;
@@ -48,62 +47,117 @@ public class ResizeAdapter extends MouseAdapter{
         	Point frameLocation = _frame.getLocationOnScreen();
         	Point currMouseLocation = e.getLocationOnScreen();
         	for (int resizeSide: _resizeSides) {
+        		
         		if (resizeSide == SwingUtilities.SOUTH) {
         			if (currMouseLocation.y >= frameLocation.y + frameSize.height - _threshold && currMouseLocation.y <= frameLocation.y + frameSize.height) {
         				_resizing = SwingUtilities.SOUTH;
         			}
         		}
 
-        		if (resizeSide == SwingUtilities.EAST) {
+        		else if (resizeSide == SwingUtilities.EAST) {
         			if (currMouseLocation.x >= frameLocation.x + frameSize.width - _threshold && currMouseLocation.x <= frameLocation.x + frameSize.width) {
         				_resizing = SwingUtilities.EAST;
         			}
         		}
 
-        		if (resizeSide == SwingUtilities.WEST) {
+        		else if (resizeSide == SwingUtilities.WEST) {
         			if (currMouseLocation.x >= frameLocation.x && currMouseLocation.x <= frameLocation.x + _threshold) {
         				_resizing = SwingUtilities.WEST;
         			}
         		}
+        		
+        		else if (resizeSide == SwingUtilities.SOUTH_EAST) {
+        			if (currMouseLocation.x >= frameLocation.x + frameSize.width - _threshold && currMouseLocation.x <= frameLocation.x + frameSize.width
+        					&& currMouseLocation.y >= frameLocation.y + frameSize.height - _threshold && currMouseLocation.y <= frameLocation.y + frameSize.height) {
+        				_resizing = SwingUtilities.SOUTH_EAST;
+        			}
+        		}
+        		
+        		else if (resizeSide == SwingUtilities.SOUTH_WEST) {
+        			
+        			if (currMouseLocation.y >= frameLocation.y + frameSize.height - _threshold && currMouseLocation.y <= frameLocation.y + frameSize.height
+        				&& currMouseLocation.x >= frameLocation.x && currMouseLocation.x <= frameLocation.x + _threshold) {
+        				_resizing = SwingUtilities.SOUTH_WEST;
+        			}
+        			
+        		}
+        		
         	}
         }
-        prevX = e.getXOnScreen();
-        prevY = e.getYOnScreen();
+        _prevX = e.getXOnScreen();
+        _prevY = e.getYOnScreen();
         _prevBounds = _frame.getBounds();
     }
 
     @Override
     public void mouseDragged (MouseEvent e) {
     	
-        if (prevX != -1 && prevY != -1 && _resizing!=0) {
-        	int xInc = e.getXOnScreen() - prevX;
-            int yInc = e.getYOnScreen() - prevY;
+        if (_prevX != -1 && _prevY != -1 && _resizing!=0) {
+        	int xInc = e.getXOnScreen() - _prevX;
+            int yInc = e.getYOnScreen() - _prevY;
             
             
             for (int resizeSide: _resizeSides) {
 
             	if (resizeSide == SwingUtilities.SOUTH && _resizing == SwingUtilities.SOUTH) {
-            		if (_prevBounds.y + _prevBounds.height + yInc <= getScreenSize().height - getScreenInsets().bottom) {
-            			_frame.setBounds(_prevBounds.x,_prevBounds.y, _prevBounds.width, _prevBounds.height + yInc);
-            		}
+            		int diff = getScreenSize().height - getScreenInsets().bottom - _prevBounds.height - _prevBounds.y;
+            		int diffInHeight = Math.min(_prevBounds.height + yInc, _prevBounds.height + diff);
+            		_frame.setBounds(_prevBounds.x,_prevBounds.y, _prevBounds.width, diffInHeight);
             	}
 
-            	if (resizeSide == SwingUtilities.EAST && _resizing == SwingUtilities.EAST) {
-            		if (_prevBounds.x + _prevBounds.width + xInc <= getScreenSize().width - getScreenInsets().right) {
-            			_frame.setBounds(_prevBounds.x,_prevBounds.y, _prevBounds.width + xInc, _prevBounds.height);
-            		}
+            	else if (resizeSide == SwingUtilities.EAST && _resizing == SwingUtilities.EAST) {
+            		int diff = getScreenSize().width - getScreenInsets().right - _prevBounds.width - _prevBounds.x;
+            		int diffInWidth = Math.min(_prevBounds.width + xInc, _prevBounds.width + diff);
+            		_frame.setBounds(_prevBounds.x,_prevBounds.y, diffInWidth, _prevBounds.height);
             	}
 
-
-            	if (resizeSide == SwingUtilities.WEST && _resizing == SwingUtilities.WEST) {
-            		if (xInc < 0 && _prevBounds.x + xInc >= 0) {
-            			_frame.setBounds(_prevBounds.x + xInc, _prevBounds.y, _prevBounds.width + xInc*-1, _prevBounds.height);
+            	else if (resizeSide == SwingUtilities.WEST && _resizing == SwingUtilities.WEST) {
+            		if (xInc < 0) {
+            			int diffInWidth = Math.max(_prevBounds.width + xInc*-1, getScreenInsets().left);
+            			_frame.setBounds(_prevBounds.x + xInc, _prevBounds.y, diffInWidth, _prevBounds.height);            			
             		}
-            		if (xInc > 0 && _frame.getSize().width - xInc >= _frame.getMinimumSize().width) {
-            			_frame.setSize(_prevBounds.width - xInc, _prevBounds.height);
-            			_frame.setLocation(_prevBounds.x + xInc, _prevBounds.y);
+            		
+            		if (xInc > 0) {
+            			int diffInLocation = Math.min(_prevBounds.x + xInc, _prevBounds.x);
+            			_frame.setLocation(diffInLocation, _prevBounds.y);
+            			if (_prevBounds.x != diffInLocation) {
+            				int diffInWidth = Math.max(_prevBounds.width - xInc, _frame.getMinimumSize().width);
+            				_frame.setSize(diffInWidth, _prevBounds.height);
+            			}
             		}
             	}
+            	
+            	else if (resizeSide == SwingUtilities.SOUTH_EAST && _resizing == SwingUtilities.SOUTH_EAST) {
+            		int diff1 = getScreenSize().height - getScreenInsets().bottom - _prevBounds.height - _prevBounds.y;
+            		int diffInHeight = Math.min(_prevBounds.height + yInc, _prevBounds.height + diff1);
+            		int diff2 = getScreenSize().width - getScreenInsets().right - _prevBounds.width - _prevBounds.x;
+            		int diffInWidth = Math.min(_prevBounds.width + xInc, _prevBounds.width + diff2);
+            		_frame.setBounds(_prevBounds.x,_prevBounds.y, diffInWidth, diffInHeight);
+            	}
+            	
+            	else if (resizeSide == SwingUtilities.SOUTH_WEST && _resizing == SwingUtilities.SOUTH_WEST) {
+            		int diff1 = getScreenSize().height - getScreenInsets().bottom - _prevBounds.height - _prevBounds.y;
+            		int diffInHeight = Math.min(_prevBounds.height + yInc, _prevBounds.height + diff1);
+            		int xBounds = _prevBounds.x;
+            		int width = _prevBounds.width;
+            		
+            		if (xInc < 0) {
+            			int diffInWidth = Math.max(_prevBounds.width + xInc*-1, getScreenInsets().left);
+            			xBounds += xInc;
+            			width = diffInWidth;      			
+            		}
+            		
+            		if (xInc > 0) {
+            			int diffInLocation = Math.min(_prevBounds.x + xInc, _prevBounds.x);
+            			xBounds = diffInLocation;
+            			if (_prevBounds.x != diffInLocation) {
+            				int diffInWidth = Math.max(_prevBounds.width - xInc, _frame.getMinimumSize().width);
+            				width = diffInWidth;
+            			}
+            		}
+            		_frame.setBounds(xBounds, _prevBounds.y, width, diffInHeight);
+            	}
+            	
             }
         }
     }
