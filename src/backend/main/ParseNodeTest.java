ab0ea67a016f262582c9ee725c3689e5743c61c5
bryceAebi <bryce_aebi@brown.edu>
2013-05-02 11:28:45 -0400

    tests for computation state strings. it works now

---
+++ src/backend/main/ParseNodeTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package backend.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author baebi
+ *
+ */
+public class ParseNodeTest {
+	 
+	
+	@Test
+	public void getComputeStringTest() {
+		ToComputeTreeNode n1 = new ToComputeTreeNode(null,null," + ");
+		ToComputeTreeNode n2 = new ToComputeTreeNode(null,null," 5.0 ");
+		ToComputeTreeNode n3 = new ToComputeTreeNode(null,null," 6.0 ");
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		ParseNode p = new ParseNode(null,null,null);
+		p.setComputeStringTreeForTesting(n1);
+		try {
+			System.out.println(p.getComputeString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
