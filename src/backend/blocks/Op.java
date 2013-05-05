8081def76c2bd23222071dad0fa947f5776afff9
bryceAebi <bryce_aebi@brown.edu>
2013-05-05 07:30:53 -0400

    small changes

--- src/backend/blocks/Op.java
+++ src/backend/blocks/Op.java
@@ -21,8 +21,7 @@ public enum Op {
 	M_INVERSE(0,true,"M-INVERSE"),
 	POWER(0,true,"POWER"),
 	M_RANK(0,true,"M-RANK"),
-	M_TRANSPOSE(0,true,"M-TRANSPOSE"),
-	NULLSPACE(0,true,"NULL-SPACE");
+	M_TRANSPOSE(0,true,"M-TRANSPOSE");
 	
 	// do not give rank below zero
 	
@@ -93,8 +92,6 @@ public enum Op {
 			return "^";
 		case M_RANK:
 			return "R";
-		case NULLSPACE:
-			return "NS";
 		case M_TRANSPOSE:
 			return "T";
 		default:
